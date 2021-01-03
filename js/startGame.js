18060dbdae9a0655c216916cc656a5ee19b04d92
bocman17771 <bocmancul@bk.ru>
2021-01-03 22:49:21 +0400

    Refactor title

--- js/startGame.js
+++ js/startGame.js
@@ -4,14 +4,14 @@ const walkerWidth = document.querySelector('.js-walker').clientWidth
 
 btnStart.onclick = () => startWalker()
 
-let sum = 0
+let step = 0
 
 async function startWalker() {
-  if(sum === frameWidth - walkerWidth){
+  if(step === frameWidth - walkerWidth){
     return
   }
-  sum += 10
-  console.log(sum)
-  walker.changeOfPositionHorizontal(sum)
+  step += 10
+  console.log(step)
+  walker.changeOfPositionHorizontal(step)
   setInterval(() => startWalker(), 1000)
 }
