8fd8e9c96ae11fc3beb30331a9461115df1e2b98
bocman17771 <bocmancul@bk.ru>
2021-01-07 18:40:42 +0400

    Fix walker start

--- js/shot.js
+++ js/shot.js
@@ -1,53 +1,53 @@
-// Coordinate designation
+// // Coordinate designation
 
-import {styleTarget} from './Target.js'
-import {styleWalker} from './Walker.js'
-import {styleField} from './Field.js'
-const field = styleField.$el
-const $btnShot = document.querySelector('.js-button_shot')
+// import {styleTarget} from './Target.js'
+// import {styleWalker} from './Walker.js'
+// import {styleField} from './Field.js'
+// const field = styleField.$el
+// const $btnShot = document.querySelector('.js-button_shot')
 
-$btnShot.onclick = () => mergerCoords({
-  target: styleTarget.$el, 
-  walker: styleWalker.$el
-})
+// $btnShot.onclick = () => mergerCoords({
+//   target: styleTarget.$el, 
+//   walker: styleWalker.$el
+// })
 
-window.addEventListener('keydown', event => {
-  if(event.code === 'Space'){
-    mergerCoords({
-      target: styleTarget.$el, 
-      walker: styleWalker.$el
-    })
-  }
-})
+// window.addEventListener('keydown', event => {
+//   if(event.code === 'Space'){
+//     mergerCoords({
+//       target: styleTarget.$el, 
+//       walker: styleWalker.$el
+//     })
+//   }
+// })
 
-function getCoords(elem) {
-  let box = elem.getBoundingClientRect()
-  return box.top + box.left
-}
+// function getCoords(elem) {
+//   let box = elem.getBoundingClientRect()
+//   return box.top + box.left
+// }
 
-function fieldChangeBackground(color) {
-  field.style.background = color
-}
+// function fieldChangeBackground(color) {
+//   field.style.background = color
+// }
 
-export let stepFinish = false
-function mergerCoords(element){ 
-  const background = fieldChangeBackground
-  const sumOne = getCoords(element.target)
-  const sumTwo = getCoords(element.walker)
-  if (sumOne === sumTwo){
-    const field = styleField.$el
-    const win = document.createElement('span')
-    win.className = 'textWin'
-    win.innerHTML = 'Win!'
-    field.append(win)
-    background('green')
-    stepFinish = true
-    setTimeout(() => {
-      win.remove()
-      background('')
-    }, 500) 
-    return
-  }
-  background('red')
-  setTimeout(() => background(''), 300)
-}
+// export let stepFinish = false
+// function mergerCoords(element){ 
+//   const background = fieldChangeBackground
+//   const targetCoords = getCoords(element.target)
+//   const walkerCoords = getCoords(element.walker)
+//   if (targetCoords === walkerCoords){
+//     const field = styleField.$el
+//     const win = document.createElement('span')
+//     win.className = 'textWin'
+//     win.innerHTML = 'Win!'
+//     field.append(win)
+//     background('green')
+//     stepFinish = true
+//     setTimeout(() => {
+//       win.remove()
+//       background('')
+//     }, 500) 
+//     return
+//   }
+//   background('red')
+//   setTimeout(() => background(''), 300)
+// }
--- js/startGame.js
+++ js/startGame.js
@@ -2,7 +2,6 @@
 
 import {styleField} from './Field.js'
 import {styleWalker} from './Walker.js'
-import {stepFinish} from './shot.js'
 const walker = styleWalker
 const walkerWidth = walker.$el.clientWidth
 const fieldWidth = styleField.$el.clientWidth
@@ -16,15 +15,74 @@ document.addEventListener('keydown', event => {
   }
 }) 
 
+let finish = false
+
 export let step = 0
 function startWalker() {
-  if(step >= fieldWidth - walkerWidth || stepFinish === true){
+  if(finish === true){
     walker.changeOfPositionHorizontal(walker.staticPosition)
+    finish = false
+    step = 0
     return
+  }
+  if(step >= fieldWidth - walkerWidth){
+    // walker.changeOfPositionHorizontal(walker.staticPosition)
+    step = 0
   }
   step += 10
   walker.changeOfPositionHorizontal(step)
-  setTimeout(() => startWalker(), 1000)
+  setTimeout(() => startWalker(), 200)
 }
 
 
+// 
+
+import {styleTarget} from './Target.js'
+const field = styleField.$el
+const $btnShot = document.querySelector('.js-button_shot')
+
+$btnShot.onclick = () => mergerCoords({
+  target: styleTarget.$el, 
+  walker: styleWalker.$el
+})
+
+window.addEventListener('keydown', event => {
+  if(event.code === 'Space'){
+    mergerCoords({
+      target: styleTarget.$el, 
+      walker: styleWalker.$el
+    })
+  }
+})
+
+function getCoords(elem) {
+  let box = elem.getBoundingClientRect()
+  return box.top + box.left
+}
+
+function fieldChangeBackground(color) {
+  field.style.background = color
+}
+
+// export let stepFinish = false
+function mergerCoords(element){ 
+  const background = fieldChangeBackground
+  const targetCoords = getCoords(element.target)
+  const walkerCoords = getCoords(element.walker)
+  if (targetCoords === walkerCoords){
+    const field = styleField.$el
+    const win = document.createElement('span')
+    win.className = 'textWin'
+    win.innerHTML = 'Win!'
+    field.append(win)
+    background('green')
+    finish = true
+    setTimeout(() => {
+      win.remove()
+      background('')
+    }, 1000) 
+    return
+  }
+  background('red')
+  setTimeout(() => background(''), 500)
+}
