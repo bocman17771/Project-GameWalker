5ced73c6ad3ecaf1c8f0305a762dcc8828bb7d69
bocman17771 <bocmancul@bk.ru>
2021-01-11 01:00:12 +0400

    Change style

--- js/Walker.js
+++ js/Walker.js
@@ -3,7 +3,6 @@ import {Style} from './Element.js'
 export const styleWalker = new Style({
   selector: '.js-walker',
   padding: 15,
-  outline: 'solid #dc3545 1px',
   position: 'absolute',
   staticPosition: 0,
   background: '#dc3545',
