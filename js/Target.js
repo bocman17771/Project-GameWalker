15357c493e06001a5047f2be1a75a0898dbdcc84
bocman17771 <bocmancul@bk.ru>
2021-01-11 23:26:09 +0400

    Change style

--- js/Target.js
+++ js/Target.js
@@ -4,9 +4,9 @@ export const styleTarget = new Style({
   selector: '.js-target',
   padding: 15,
   outline: 'solid #ffc107 3px',
-  // backgroundImage: 'url("../assets/img/svg/target.svg")',
-  // backgroundPosition: 'center',
-  // backgroundSize: '100%',
+  backgroundImage: 'url("../assets/img/svg/target2.svg")',
+  backgroundPosition: 'center',
+  backgroundSize: '100%',
   position: 'absolute',
   staticPosition: 90,
 })
